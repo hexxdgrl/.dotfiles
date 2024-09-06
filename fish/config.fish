9e72c2cfd3f9a76bf8d47f18a9a1d084d51f5cf0
hexxdgrl <hexxdgrl@github.com>
2024-09-06 10:29:10 -0600

    fresh conf

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,3 @@
-set fish_greeting "/ᐠ - ˕ -マx"
-
-function fish_prompt
-    string join ' ' -- (set_color c9545d)$PWD (set_color normal)o'⥼'
-
-
-end
+set fish_greeting "/ᐠ - ˕ -マ"
 
 set VISUAL 'zed'
