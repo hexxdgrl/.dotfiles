3d8e6aad3e9eb2c8ab8a7e35c9d3ec28b289d149
hexxdgrl <hexxdgrl@github.com>
2024-09-26 16:46:10 -0600

    brought in line with desktop

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,3 @@
-set fish_greeting "/ᐠ - ˕ -マx"
+set fish_greeting "Haiii!!!"
 
-function fish_prompt
-    string join ' ' -- (set_color c9545d)$PWD (set_color normal)o'⥼'
-
-
-end
-
-set VISUAL 'zed'
+set VISUAL 'zeditor'
